ce9b96eb55e6fe767feef651331a1d9ea1e41778
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-05 20:08:09 +0200

    added generate random string function in register api

--- register_api.php
+++ register_api.php
@@ -9,5 +9,15 @@ $user_name_err = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['password'])) {
+        function generateRandomString($length = 10)
+        {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
     }
 }
