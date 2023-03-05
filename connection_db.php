e15f6d216d92abac35f7a97dc78544b5653fa724
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-05 17:18:46 +0200

    added connectiom.php file

---
+++ connection_db.php
@@ -0,0 +1,11 @@
+<?php
+$host_name = 'localhost';
+$database = 'techlab_db';
+$username = 'root';
+$password = '';
+
+$link = new mysqli($host_name, $username, $password, $database);
+
+if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: ' . $link->connect_error . '</p>');
+}
