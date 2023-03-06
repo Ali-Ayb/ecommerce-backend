154bf53edb0bf5c0a5b2a82b08646f650077f7ae
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-06 20:15:17 +0200

    Added an API for accessing products details from the database.

--- list_users_api.php
+++ list_users_api.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 include('connection_db.php');
 
-$sql = "select user_id, first_name, last_name,age,gender,email from users";
+$sql = "select * from users";
 
 $result = mysqli_query($link,$sql);
 $users = mysqli_fetch_all($result,MYSQLI_ASSOC);
