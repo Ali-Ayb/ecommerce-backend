f8f057a18cd373825e617f1d15e8d83a22d363bc
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-06 14:30:26 +0200

    added product api functinoality

---
+++ add_product_api.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('connection_db.php');
+
+// $product_id = $_POST['product_id'];
+$product_name = $_POST['product_name'];
+$product_brand = $_POST['product_brand'];
+$product_price = $_POST['product_price'];
+$product_img = $_POST['product_img'];
+$product_category = $_POST['product_category'];
+$product_description = $_POST['product_description'];
+$product_model_number = $_POST['product_model_number'];
+$product_quantity = $_POST['product_quantity'];
+
+$sql = "INSERT INTO products (product_name, product_brand, product_price, product_img, product_category, product_description, product_model_number, product_quantity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "ssissssi", $product_name, $product_brand, $product_price, $product_img, $product_category, $product_description, $product_model_number, $product_quantity);
+$result = mysqli_stmt_execute($stmt);
+
+$response = [
+    'response' => $result
+];
+echo json_encode($response);
