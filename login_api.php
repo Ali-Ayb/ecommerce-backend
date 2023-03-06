9f069a51285611129c3e83b5ed062562525ca8c2
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-06 13:53:17 +0200

    added login api functionality

--- login_api.php
+++ login_api.php
@@ -7,3 +7,76 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 }
 
 require_once "connection_db.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+
+        function validate($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        $email = validate($_POST['email']);
+        $password = validate($_POST['password']);
+
+        if (empty($email)) {
+            $err =  'email is empty';
+            $response = [
+                "response" => $err
+            ];
+            echo json_encode([$response]);
+
+            exit();
+        } else if (empty($password)) {
+            $err =  'Password is required"';
+            $response = [
+                "response" => $err
+            ];
+            echo json_encode([$response]);
+
+            exit();
+        } else {
+            $sql = "SELECT * FROM users WHERE email='$email'";
+            $result = mysqli_query($link, $sql);
+
+            if (mysqli_num_rows($result) === 1) {
+                $row = mysqli_fetch_assoc($result);
+                $password = hash('sha256', $password . $row['salt']);
+
+                if ($row['email'] === $email && $row['password'] === $password) {
+                    echo "Logged in!";
+                    $_SESSION['email'] = $row['email'];
+                    $_SESSION['loggedin'] = true;
+                } else {
+                    $err =  'Password is required"';
+                    $response = [
+                        "response" => $err
+                    ];
+                    echo json_encode([$response]);
+                }
+            } else {
+                $err =  'failed"';
+                $response = [
+                    "response" => $err
+                ];
+                echo json_encode([$response]);
+            }
+        }
+    } else {
+
+        $err =  'failed"';
+        $response = [
+            "response" => $err
+        ];
+        echo json_encode([$response]);
+    }
+} else {
+    $err =  'failed"';
+    $response = [
+        "response" => $err
+    ];
+    echo json_encode([$response]);
+}
